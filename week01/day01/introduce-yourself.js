6e9275b199d35625bbded27bee1e65d09b948e7a
Xiaoxing chen <775132026@qq.com>
2019-10-20 22:41:58 +0800

    day01 exercise

---
+++ week01/day01/introduce-yourself.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+function intro(name, age, height){
+    console.log(name);
+    console.log(age);
+    console.log(height);
+}
+//
+//  Example output
+//  John Doe
+//  31
+//  1.87
+intro('John Doe', 31, 1.87);
