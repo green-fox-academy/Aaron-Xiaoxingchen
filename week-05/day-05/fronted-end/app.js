bec75493ee86c949cbfcedaef61aef6cfcfdd755
xiaoxing <775132026@qq.com>
2019-11-17 21:35:56 +0800

    finished array Handler

--- week-05/day-05/fronted-end/app.js
+++ week-05/day-05/fronted-end/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.json());
 app.use(express.static('assets'));
 
 app.get('/', (req, res) =>{
@@ -43,28 +44,23 @@ app.get('/greeter', (req, res) =>{
   }
 })
 
-app.get('/appenda:/appendable', (req, res) => {
+app.get('/appenda/:appendable', (req, res) => {
   const { appendable } = req.params;
   res.send({ appended: `${appendable}a` })
 })
 
-function factor(n){
-  while(n >= 1){
-    n*(n-1);
-    n--;
-  }
-}
 app.post('/dountil/:action', function (req, res) {
   const{action} = req.params;
-  const {param} = req.body;
-  if(param){
+  let {until} = req.body;
+  until = parseInt(until, 10);
+  if(until){
     if(action === 'sum'){
       res.send({
-        result: 3* param
+        result: 3* until
       });
     }else if(action === 'factor'){
       res.send({
-        result: factor(param)
+        result: factor(until)
       })
     }
   }else{
@@ -73,6 +69,34 @@ app.post('/dountil/:action', function (req, res) {
     })
   }
 });
+
+function factor(n){
+  var sum = 1;
+  while(n >= 1){
+    sum = sum * n;
+    n--;
+  }
+  return sum;
+}
+
+app.post('/arrays', function(req, res){
+  const {what, numbers} = req.body;
+  let result = null;
+  switch(what){
+    case 'sum':
+      result = {result: numbers.reduce((a,b) => a + b)};
+      break;
+    case 'multiply':
+      result = {result: numbers.reduce((a ,b) => a*b)};
+      break;
+    case 'double':
+      result = {result: numbers.map((a) => a * 2)};
+      break;
+    default:
+      result = {"error": "Please provide what to do with the numbers!"};
+  }
+  res.send(result);
+})
 
 app.listen('3000', ()=> 
   console.log('the app is listening 3000'))
