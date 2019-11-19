0eafa16a91a4bc85a85068875d905283a35170a8
xiaoxing <775132026@qq.com>
2019-11-19 23:01:10 +0800

    bugs

--- week-06/day-02/user/app.js
+++ week-06/day-02/user/app.js
@@ -1,13 +1,35 @@
-const express = require('express');
-const app = express();
-const Mysql = require('promise-mysql');
+const Mysql = require('mysql');
+const fs = require('fs');
+const csv = require('fast-csv');
 
-MySQL.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'chen06602',
-  database: 'bookstore'
-}).then(value => (pconn = value));
+let stream = fs.createReadStream('users.csv');
+let myData = [];
+let csvStream = csv
+  .parse()
+  .on("data", function(data){
+    myData.push(data);
+  })
+  .on("end", function(){
+    myData.shift();
 
-const statement = `INSERT INTO users (`prefix`,`first_name`,`last_name`,`address`,`height`,`bitcoin_address`,`color_preference`) VALUES ?,?,?,?,?,?,?`;
+    const connection = Mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'chen06602',
+      database: 'bookstore'
+    })
+
+    connection.connect((error) =>{
+      if(error){
+        console.error(error);
+      }else{
+        let query = `INSERT INTO users (prefix,first_name,last_name,address,height,bitcoin_address,color_preference) VALUES ?`;
+        connection.query(query, [myData], (error, response) =>{
+          console.log(error || response)
+        })
+      }
+    })
+  })
+
+stream.pipe(csvStream);
